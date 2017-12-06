a6d05d8132e173bfa0d99967666825aea6d8bb39
adi <adi@work>
2017-12-06 16:56:17 +0100

    - fop

--- bordereau/src/com/bordereau/view/MainViewController.java
+++ bordereau/src/com/bordereau/view/MainViewController.java
@@ -5,16 +5,20 @@ import com.bordereau.data.Mandats;
 import com.bordereau.model.Auteur;
 import com.bordereau.model.Document;
 import com.bordereau.model.Mandat;
+import com.bordereau.model.XmlPrint;
 import com.bordereau.utils.Log;
-
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.StringWriter;
 
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
@@ -28,22 +32,24 @@ import javafx.scene.control.TableView;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
-
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.transform.Result;
 import javax.xml.transform.Transformer;
+
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-
-import org.apache.fop.*;
 import org.apache.fop.apps.FOPException;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.xmlgraphics.util.MimeConstants;
+
 
 import org.xml.sax.SAXException;
 
@@ -78,6 +84,18 @@ public class MainViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+        try {
+            print();
+        } catch (IOException ex) {
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JAXBException ex) {
+            Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         date.setValue(LocalDate.now());
         
         //Definition colonnes
@@ -164,10 +182,10 @@ public class MainViewController implements Initializable {
     }
     
     @FXML
-    private void print() throws FileNotFoundException, FOPException, TransformerConfigurationException, TransformerException, IOException, SAXException{
-        
-        /*XmlPrint printDoc = new XmlPrint(listDocuments);
-        
+    private void print() throws IOException, FOPException, SAXException, TransformerConfigurationException, TransformerException, JAXBException{
+        /*
+        XmlPrint printDoc = new XmlPrint(listDocuments);
+
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(XmlPrint.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
@@ -179,11 +197,11 @@ public class MainViewController implements Initializable {
         } catch (JAXBException ex) {
             Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
         }*/
-        //https://netjs.blogspot.ch/2015/07/how-to-create-pdf-from-xml-using-apache-fop.html
         
-        File xsltFile = new File("C:/Temp/template.xsl");
-        StreamSource xmlSource = new StreamSource(new File("C:/Temp/Employees.xml"));
-        FopFactory fopFactory = FopFactory.newInstance(new File("C:/Temp/fop.xconf"));
+        File xsltFile = new File("C:/Temp/AMVtemplate.xsl");
+        StreamSource xmlSource = new StreamSource(new File("C:/Temp/AMVdocuments.xml"));
+        //FopFactory fopFactory = FopFactory.newInstance(new File("C:/Temp/fop.xconf"));
+        FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
         FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
         OutputStream out;
         out = new java.io.FileOutputStream("C:/Temp/employee.pdf");
@@ -204,8 +222,8 @@ public class MainViewController implements Initializable {
             transformer.transform(xmlSource, res);
         } finally {
             out.close();
-        }        
+        }           
     }    
 }
 
-//Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+        
